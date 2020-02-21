ceac56e53213d4367d42cf109280de01315657c6
yikezawa <y.ikezawa93@gmail.com>
2020-02-21 22:47:30 +0900

    mod

--- handlers/write_ext/main_test.go
+++ handlers/write_ext/main_test.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"io/ioutil"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	inputJson := readJsonFromFile(t, "../../testdata/s3event.json")
+	snsEvent := events.SNSEvent{
+		Records: []events.SNSEventRecord{
+			{
+				SNS: events.SNSEntity{
+					Message: string(inputJson)},
+			},
+		},
+	}
+	snsEvnetByte, err := json.Marshal(snsEvent)
+	if err != nil {
+		t.Errorf("error: %s", err)
+	}
+
+	sqsEvent := events.SQSEvent{
+		Records: []events.SQSMessage{
+			{
+				Body: string(snsEvnetByte),
+			},
+		},
+	}
+	if err := handler(sqsEvent); err != nil {
+		t.Errorf("error: %s", err)
+	}
+}
+
+func readJsonFromFile(t *testing.T, inputFile string) []byte {
+	inputJson, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		t.Errorf("could not open test file. details: %v", err)
+	}
+
+	return inputJson
+}
